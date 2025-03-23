669d9ad9ad9bc00db32ec62602c0dc946804606e
muriloleal13 <msleal@inf.ufsm.br>
2025-03-23 10:35:47 -0300

    chore: base setup using Quasar Framework and TailwindCSS

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page
+    class="flex flex-center min-h-screen bg-gradient-to-br from-purple-400 to-blue-500"
+  >
+    <div class="text-center">
+      <h1 class="text-xl font-bol text-red-500">Tailwind v4 + Quasar 🎉</h1>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
